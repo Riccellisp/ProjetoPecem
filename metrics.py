0b1cd583c69b8b8b561577b4d1dc1aea92fb5eb9
Bruno Riccelli dos Santos Silva <riccellisp456@hotmail.com>
2022-10-11 17:46:03 -0300

    Creating metrics.py with mse and ssim metrics

---
+++ metrics.py
@@ -0,0 +1,18 @@
+from skimage.metrics import structural_similarity as ssim
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+
+def mse(imageA, imageB):
+	# the 'Mean Squared Error' between the two images is the
+	# sum of the squared difference between the two images;
+	# NOTE: the two images must have the same dimension
+	err = np.sum((imageA.astype("float") - imageB.astype("float")) ** 2)
+	err /= float(imageA.shape[0] * imageA.shape[1])
+	
+	# return the MSE, the lower the error, the more "similar"
+	# the two images are
+	return err
+
+def ssim (imageA, imageB):
+  return ssim(imageA, imageB)
