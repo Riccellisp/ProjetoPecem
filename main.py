8f869fba1f67191c6cc253eb74427bab5c398da7
pedro <pedro.motta@dellead.com>
2022-10-12 12:21:05 -0300

    added examples.ipynb to run metrics. added images folder for images used in examples.ipynb. added AMBE and IEM metrics

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
 import cv2
 from metrics import mse,ssim
+
+import numpy as np
+from metrics import AMBE
+
+# AMBE
+imageA = cv2.imread('images/Fig0310(b)(washed_out_pollen_image).tif',0)
+imageB = cv2.equalizeHist(imageA)
+
+ambe = AMBE(imageA, imageB)
+print(f'AMBE is: {ambe}')
+
+horizontal_concat = np.concatenate((imageA, imageB), axis=1)
+cv2.imshow("Horizontal plot", horizontal_concat)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
--- metrics.py
+++ metrics.py
@@ -16,3 +16,55 @@ def mse(imageA, imageB):
 
 def ssim (imageA, imageB):
   return ssim(imageA, imageB)
+
+def AMBE(imageA, imageB):
+	'''
+	AMBE is the abbreviation of Absolute Mean Brightness Error. It is the absolute 
+	difference between the mean of input and output image. It is formally defined by:
+
+							AMBE = | E(X) B E(Y) |
+
+	where, X and Y denote the input and output image, respectively and E(.) denotes
+	the expected value, i.e., the statistical mean. 
+	Equation 1 clearly shows that AMBE is designed to detect one of the distortions-
+	excessive brightness change. AMBE was proposed by Chen and Ramli (2003b) to evaluate 
+	the performance in preserving original brightness.
+
+	imageA is the raw image (before histogram equalization per example), and imageB 
+	is the image after preprocessing
+	'''
+
+	return np.abs(np.mean(imageA) - np.mean(imageB))
+
+def IEM_filter(image):
+	image = image.astype(np.float32)
+
+	(iH, iW) = image.shape[:2]
+	output = np.zeros((iH-2, iW-2), dtype="float32")
+
+	for y in np.arange(0, iH-2):
+		for x in np.arange(0, iW-2):
+
+			roi = image[y:y + 3, x:x + 3]
+
+			k = roi
+			k = (np.abs(k[1][1] - k[0][0]) + np.abs(k[1][1] - k[0][1]) + np.abs(k[1][1] - k[0][2]) +
+				np.abs(k[1][1] - k[1][0]) + np.abs(k[1][1] - k[1][2]) +
+				np.abs(k[1][1] - k[2][0]) + np.abs(k[1][1] - k[2][1]) + np.abs(k[1][1] - k[2][2]))  
+
+			output[y, x] = k
+	return np.sum(output)
+
+def IEM(imageA, imageB):
+	'''
+	Image Enhancement Metric(IEM) approximates the contrast and
+	sharpness of an image by dividing an image into non-overlapping
+	blocks. 
+
+	imageA is the raw image (before histogram equalization per example), and imageB 
+	is the image after preprocessing
+	'''
+	valA = IEM_filter(imageA)
+	valB = IEM_filter(imageB)
+
+	return valB/valA
