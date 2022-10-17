c9c246e3aae68988302c73a3fc63807a3cfde56e
CarlosAlfredo <56643645+Carlos-Alfredo@users.noreply.github.com>
2022-10-17 20:35:28 -0300

    Implementação EME, RMSE e EMEE.

--- metrics.py
+++ metrics.py
@@ -3,6 +3,7 @@ from skimage import color
 import matplotlib.pyplot as plt
 import numpy as np
 import cv2
+import math
 
 def mse(imageA, imageB):
 	# the 'Mean Squared Error' between the two images is the
@@ -36,6 +37,158 @@ def AMBE(imageA, imageB):
 	'''
 
 	return np.abs(np.mean(imageA) - np.mean(imageB))
+
+def eme(img,rowSample,columnSample):
+	
+	grayImg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+	
+	rowSize, columnSize = grayImg.shape
+	nRows = int(rowSize/rowSample)
+	nColumns = int(columnSize/columnSample)
+	incompleteRow = math.ceil(rowSize/rowSample) - nRows
+	incompleteColumn = math.ceil(columnSize/columnSample) - nColumns
+
+	somatory = 0
+	nBlocks = nRows*nColumns
+	for i in range(0,nRows):
+		for j in range(0,nColumns):
+			imax=0
+			imin=255
+			for x in range(i*rowSample,(i+1)*rowSample):
+				for y in range(j*columnSample,(j+1)*columnSample):
+					if grayImg[x,y]>imax:
+						imax=grayImg[x,y]
+					if grayImg[x,y]<imin:
+						imin=grayImg[x,y]
+			if imin==0:
+				somatory = somatory + 20*math.log(imax/0.5)
+			else:
+				somatory = somatory + 20*math.log(imax/imin)
+
+	if incompleteColumn==1:
+		for i in range(0,nRows):
+			imax=0
+			imin=255
+			for x in range(i*rowSample,(i+1)*rowSample):
+				for y in range(nColumns*columnSample,columnSize):
+					if grayImg[x,y]>imax:
+						imax=grayImg[x,y]
+					if grayImg[x,y]<imin:
+						imin=grayImg[x,y]
+			if imin==0:
+				somatory = somatory + 20*math.log(imax/0.5)
+			else:
+				somatory = somatory + 20*math.log(imax/imin)
+			nBlocks = nBlocks + 1
+	if incompleteRow==1:
+		for j in range(0,nColumns):
+			imax=0
+			imin=255
+			for x in range(nRows*rowSample,rowSize):
+				for y in range(j*columnSample,(j+1)*columnSample):
+					if grayImg[x,y]>imax:
+						imax=grayImg[x,y]
+					if grayImg[x,y]<imin:
+						imin=grayImg[x,y]
+			if imin==0:
+				somatory = somatory + 20*math.log(imax/0.5)
+			else:
+				somatory = somatory + 20*math.log(imax/imin)
+			nBlocks = nBlocks + 1
+	if incompleteRow==1 and incompleteColumn==1:
+		imax=0
+		imin=255
+		for x in range(nRows*rowSample,rowSize):
+			for y in range(nColumns*columnSize,columnSize):
+				if grayImg[x,y]>imax:
+					imax=grayImg[x,y]
+				if grayImg[x,y]<imin:
+					imin=grayImg[x,y]
+		if imin==0:
+			somatory = somatory + 20*math.log(imax/0.5)
+		else:
+			somatory = somatory + 20*math.log(imax/imin)
+		nBlocks = nBlocks + 1
+	return somatory/nBlocks
+
+def rmse(img1,img2):
+	error = np.subtract(img1,img2)
+	sqrtError = np.square(error)
+	meanSqrtError = np.mean(sqrtError)
+	return math.sqrt(meanSqrtError)
+
+def emee(img,rowSample,columnSample):
+	
+	grayImg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+	
+	rowSize, columnSize = grayImg.shape
+	nRows = int(rowSize/rowSample)
+	nColumns = int(columnSize/columnSample)
+	incompleteRow = math.ceil(rowSize/rowSample) - nRows
+	incompleteColumn = math.ceil(columnSize/columnSample) - nColumns
+
+	somatory = 0
+	nBlocks = nRows*nColumns
+	for i in range(0,nRows):
+		for j in range(0,nColumns):
+			imax=0
+			imin=255
+			for x in range(i*rowSample,(i+1)*rowSample):
+				for y in range(j*columnSample,(j+1)*columnSample):
+					if grayImg[x,y]>imax:
+						imax=grayImg[x,y]
+					if grayImg[x,y]<imin:
+						imin=grayImg[x,y]
+			if imin==0:
+				somatory = somatory + imax/0.5*math.log(imax/0.5)
+			else:
+				somatory = somatory + imax/imin*math.log(imax/imin)
+
+	if incompleteColumn==1:
+		for i in range(0,nRows):
+			imax=0
+			imin=255
+			for x in range(i*rowSample,(i+1)*rowSample):
+				for y in range(nColumns*columnSample,columnSize):
+					if grayImg[x,y]>imax:
+						imax=grayImg[x,y]
+					if grayImg[x,y]<imin:
+						imin=grayImg[x,y]
+			if imin==0:
+				somatory = somatory + imax/0.5*math.log(imax/0.5)
+			else:
+				somatory = somatory + imax/imin*math.log(imax/imin)
+			nBlocks = nBlocks + 1
+	if incompleteRow==1:
+		for j in range(0,nColumns):
+			imax=0
+			imin=255
+			for x in range(nRows*rowSample,rowSize):
+				for y in range(j*columnSample,(j+1)*columnSample):
+					if grayImg[x,y]>imax:
+						imax=grayImg[x,y]
+					if grayImg[x,y]<imin:
+						imin=grayImg[x,y]
+			if imin==0:
+				somatory = somatory + imax/0.5*math.log(imax/0.5)
+			else:
+				somatory = somatory + imax/imin*math.log(imax/imin)
+			nBlocks = nBlocks + 1
+	if incompleteRow==1 and incompleteColumn==1:
+		imax=0
+		imin=255
+		for x in range(nRows*rowSample,rowSize):
+			for y in range(nColumns*columnSize,columnSize):
+				if grayImg[x,y]>imax:
+					imax=grayImg[x,y]
+				if grayImg[x,y]<imin:
+					imin=grayImg[x,y]
+		if imin==0:
+			somatory = somatory + imax/0.5*math.log(imax/0.5)
+		else:
+			somatory = somatory + imax/imin*math.log(imax/imin)
+		nBlocks = nBlocks + 1
+	return somatory/nBlocks
 
 def IEM_filter(image):
 	image = image.astype(np.float32)
